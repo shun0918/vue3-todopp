e955e9994e5a5bbdd0f0bc7adaa90e26cb2bcbed
shun0918 <kkabosu386@gmail.com>
2021-05-31 12:36:01 +0900

    フォーマッタのテスト

--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -1,10 +1,14 @@
 <template>
-  <h1>Hello World!</h1>
+  <h1 class="title">Hello World!</h1>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-
 export default defineComponent({
   setup() {},
 });
 </script>
+<style>
+.title {
+  color: #f00;
+}
+</style>

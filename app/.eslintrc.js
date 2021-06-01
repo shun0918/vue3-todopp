55707dc5b8a1fec2ef4f3fe8e7807130e8d7d1da
shun0918 <kkabosu386@gmail.com>
2021-06-01 18:46:49 +0900

    ESLint周りを整備

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,16 +1,30 @@
+/* eslint-env node */
+const path = require('path');
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:vue/vue3-recommended', '@vue/typescript', 'airbnb-base', 'prettier', 'eslint-config-prettier'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'prettier',
+    'eslint-config-prettier',
+  ],
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint'],
+  plugins: ['vue', '@typescript-eslint', 'import'],
   settings: {
-    'import/resolver': { config: path.join(__dirname, 'webpack.config.js') },
+    'import/resolver': {
+      webpack: { config: path.join(__dirname, 'webpack.config.js') },
+    },
+  },
+  rules: {
+    'import/extensions': ['.js', '.json', '.ts', '.vue'],
   },
 };
